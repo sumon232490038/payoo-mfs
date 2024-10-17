62d3d58d2fbd678a74e89470de7be630cc4fa079
sumon232 <sumonmia4526@gmail.com>
2024-10-17 20:41:06 +0600

    payoo mfs accBalace sum done

--- js/home.js
+++ js/home.js
@@ -1,3 +1,28 @@
-const addMoneyBtn = document.getElementById('add-money-btn').addEventListener("click",function(evey){
-    console.log("js conneted to home page")
+
+// step -1====
+const addMoneyBtn = document.getElementById('add-money-btn').addEventListener("click",function(event){
+   event.preventDefault()
+   
+const pinNumberInput =document.getElementById('pin-number-input').value
+const amountInput = document.getElementById('amount-input').value
+
+
+
+if(pinNumberInput === '123'){
+
+const accountBalance = document.getElementById('account-balance').innerText 
+  const amountMakeInt = parseFloat(amountInput)
+  const accountAmountInt = parseFloat(accountBalance)
+  const newBalance= amountMakeInt + accountAmountInt
+  console.log(newBalance)
+  
+ document.getElementById('account-balance').innerText = newBalance
+
+
+}
+else{
+    alert('somethin was wrong bro !')
+}
+
+   
 })
